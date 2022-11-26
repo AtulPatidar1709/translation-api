ac1bbd89553b1cd6a7d960dfe6f2a2a131fdebc1
AtulPatidar <atulpatidar1709@gmail.com>
2022-11-26 11:56:41 +0530

    Working Properly

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 
 var inputBox = document.querySelector("#input-text");
-var translateBtn = document.querySelector("#translate-btn");
+var translateBtn1 = document.querySelector("#translate-btn1");
+var translateBtn2 = document.querySelector("#translate-btn2");
+
 var outputText = document.querySelector("#output-text");
 
 const baseURL = "https://api.funtranslations.com/translate/minion.json";
 
+const secondURL = "https://api.funtranslations.com/translate/emoji.json";
+
 function getTranslationURL(inputText) {
     return baseURL + "?" + "text=" + inputText;
+}
+
+function getTranslationURL2(inputText) {
+    return secondURL + "?" + "text=" + inputText;
 }
 
 function handleError(error) {
@@ -25,5 +33,18 @@ function translateText() {
 
 }
 
+function translateText2() {
 
-translateBtn.addEventListener("click", translateText);
+    var inputText = inputBox.value;
+
+    fetch(getTranslationURL2(inputText))
+        .then(response => response.json())
+        .then(json => outputText.innerHTML = json.contents.translated)
+        .catch(handleError);
+
+}
+
+
+
+translateBtn1.addEventListener("click", translateText);
+translateBtn2.addEventListener("click", translateText2);
