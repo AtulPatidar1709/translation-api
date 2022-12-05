8c97bc60b04c9cce1890dc37c36c5a825a4eb23d
AtulPatidar1709 <Atulpatidar1709@gmail.com>
2022-12-06 00:19:18 +0530

    Main Changes for NeoG Camp

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
 
 var inputBox = document.querySelector("#input-text");
-var translateBtn1 = document.querySelector("#translate-btn1");
 var translateBtn2 = document.querySelector("#translate-btn2");
-
 var outputText = document.querySelector("#output-text");
 
-const baseURL = "https://api.funtranslations.com/translate/minion.json";
-
 const secondURL = "https://api.funtranslations.com/translate/roman-numerals.json";
-
-function getTranslationURL(inputText) {
-    return baseURL + "?" + "text=" + inputText;
-}
 
 function getTranslationURL2(inputText) {
     return secondURL + "?" + "text=" + inputText;
 }
 
 function handleError(error) {
-    alert("Sorry there's an issue! Please try again later");
+    alert("Please Enter Number & try again");
     console.log(error);
-}
-
-function translateText() {
-
-    var inputText = inputBox.value;
-
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => outputText.innerHTML = json.contents.translated)
-        .catch(handleError);
-
 }
 
 function translateText2() {
@@ -44,7 +25,4 @@ function translateText2() {
 
 }
 
-
-
-translateBtn1.addEventListener("click", translateText);
 translateBtn2.addEventListener("click", translateText2);
